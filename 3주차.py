f93f27b9874e41f8eb663ebb179956c66726d010
dongeung0410 <163506979+dongeung0410@users.noreply.github.com>
2024-09-20 19:47:30 +0900

    Create 3주차.py
    3주차 과제입니다.

---
+++ 3주차.py
@@ -0,0 +1,25 @@
+# 1번 문제
+string="Hello,world!"
+print(string[0:-1])
+
+# 2번 문제
+result=[]
+a=7
+b=3
+c=((a+b)**2)
+result.append(c)
+print(result)
+
+# 3번 문제
+result=[]
+results=[1,2,3,4,5]
+result.append(results[4])
+result.append(results[3])
+result.append(results[2])
+result.append(results[1])
+result.append(results[0])
+print(result)
+
+# 4번 문제
+dic={"apple":3,"banana":5,"cherry":2}
+print(dic["banana"])
